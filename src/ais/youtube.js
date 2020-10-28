68adde78885d0e954bb25adf8e17ed159f9fe0c2
Cody <hoban.j.cody@gmail.com>
2020-10-27 22:31:02 -0400

    adding api

--- src/ais/youtube.js
+++ src/ais/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios'
+
 const KEY = 'AIzaSyBDjwEQ0Sk4hajudQawLthgosUZlfHTrg0';
+
+export default axios.create({
+   baseURL: 'https://www.googleapis.com/youtube/v3',
+   params: {
+       part: 'snippet',
+       type: 'video',
+       maxResults: 5,
+       key: KEY
+   }
+});
